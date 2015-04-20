aca14c7977430286ec466e46bfc8508c03706645
radiodario <dario.villanueva@gmail.com>
2015-04-20 16:59:27 +0100

    boince

--- src/BlueprintOutputSentimentTweets.js
+++ src/BlueprintOutputSentimentTweets.js
@@ -100,15 +100,16 @@ BlueprintOutputSentimentTweets.prototype.outputTweet = function(tweet) {
   self.world.addPickable(mesh, tweet.id_str);
 
   tweet.mesh = mesh;
-  // remove the object in 5 seconds
+  tweet.spawnedAt = new Date().getTime();
+  // add a times bounced counted
+  tweet.bounced = 0;
+
   self.tweets.push(tweet);
 
   VIZI.Messenger.on("pick-hover:" + tweet.id_str, function() {
     if (self.hidden) {
       return;
     }
-
-    tweet.stop = true;
 
     console.log("pick-hover:" + tweet.id_str)
 
@@ -143,8 +144,6 @@ BlueprintOutputSentimentTweets.prototype.outputTweet = function(tweet) {
       self.remove(self.pickedMesh);
     }
 
-    tweet.stop = false;
-
   });
 
 
@@ -159,7 +158,7 @@ BlueprintOutputSentimentTweets.prototype.outputTweet = function(tweet) {
 
 BlueprintOutputSentimentTweets.prototype.onTick = function() {
   var self = this;
-  var i, l, tw, elapsed;
+  var i, l, tw, elapsed, angle;
 
   var t = new Date().getTime();
 
@@ -168,13 +167,7 @@ BlueprintOutputSentimentTweets.prototype.onTick = function() {
     for (var l = self.tweets.length, i = l - 1; i > 0; i--) {
       tw = self.tweets[i];
 
-      elapsed = t - new Date(tw.created_at).getTime();
-
-      if (!tw.stop) {
-        tw.mesh.position.y = Math.abs(Math.sin(elapsed*0.001)*20);
-      }
-
-      tw.mesh.lookAt(self.world.camera.camera.position);
+      elapsed = t - tw.spawnedAt;
 
       // remove the ones aged more than 5 minutes;
       // XXX Make this a constant / configurable;
@@ -182,7 +175,24 @@ BlueprintOutputSentimentTweets.prototype.onTick = function() {
         self.remove(tw.mesh);
         self.world.removePickable(tw.mesh, tw.id_str);
         self.tweets.splice(i, 1);
+        continue;
       }
+
+      tw.mesh.lookAt(self.world.camera.camera.position);
+
+
+      if (!tw.stop) {
+        angle = elapsed*0.001;
+        tw.mesh.position.y = Math.abs(Math.sin(angle)*20);
+
+        // only bounce 3 times
+        if (Math.floor(angle / Math.PI) > 2) {
+          tw.stop = true;
+        }
+      }
+
+
+
 
       // remove them by age
     }
